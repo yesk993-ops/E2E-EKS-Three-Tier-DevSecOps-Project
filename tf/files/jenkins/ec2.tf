0c6a0f00d13a7c28c5bfd687d5cfd823bfb22c05
yesk993-ops <yesk993@gmail.com>
2026-04-01 09:52:35 +0530

    Update ec2.tf

--- tf/files/jenkins/ec2.tf
+++ tf/files/jenkins/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.ami.image_id
-  instance_type          = "t2.xlarge"
+  instance_type          = "t4g.xlarge"
   key_name               = var.key-name
   subnet_id              = aws_subnet.public-subnet.id
   vpc_security_group_ids = [aws_security_group.security-group.id]
@@ -8,7 +8,7 @@ resource "aws_instance" "ec2" {
   root_block_device {
     volume_size = 30
   }
-  user_data = templatefile("./tools-install.sh", {})
+  user_data = templatefile("./tools-install.sh(ARM)", {})
 
   tags = {
     Name = var.instance-name
